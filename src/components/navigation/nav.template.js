d8e309e006ab39de73bbb96c7d6681cd2fb85dec
Mudreuski <bahus1994@mail.ru>
2018-12-23 18:47:50 +0300

    feat: add healts bar in battle screen

--- src/components/navigation/nav.template.js
+++ src/components/navigation/nav.template.js
@@ -10,27 +10,13 @@ export default `
         <li class="nav-item active">
           <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Landing</a>
-        </li>
         <li class="nav-item js-call-modal">
         <button type="button" class="btn btn-primary js-start-game">
         Start game
         </button>
          </li>
         <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu" aria-labelledby="dropdown01">
-            <button type="button" class="dropdown-item  btn btn-primary js-choose-player-name-nav">
-            Choose Player Name
-            </button>
-            
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-          </div>
+          <a class="nav-link active" href="#">Tabble</a>
         </li>
       </ul>
       <button type="button" class="btn btn-primary js-show-player-name js-choose-player-name-nav">
--- src/screens/battle/battle.template.js
+++ src/screens/battle/battle.template.js
@@ -4,12 +4,10 @@ export default `
     <div class="col-sm">
       <div id="player">
         <div class="player-card js-player-card" style="width: 18rem;">
-          <img class="card-img-top" src="https://img.craftpix.net/2018/05/Raccoon-Sheep-and-Cat-2D-Game-Sprites.gif" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title js-name">Player</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+          <h5 class="card-title js-name">Player</h5>
+          <progress value="80" max="100"></progress>
+          <div class="playerHealts"></div>
+          <img class="card-img-top" src="https://thumbs.gfycat.com/HopefulEminentAsianpiedstarling-max-1mb.gif" alt="Card image cap">
         </div>
       </div>
     </div>
@@ -19,12 +17,9 @@ export default `
     <div class="col-sm">
       <div id="monster">
         <div class="monster-card" style="width: 18rem;">
-          <img class="card-img-top" src=https://img.craftpix.net/2018/10/Free-Orc-Ogre-and-Goblin-Chibi-2D-Game-Sprites1.gif alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">Monster</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-          </div>
+          <h5 class="card-title js-name">Monster</h5>
+          <progress value="100" max="100"></progress>
+          <img class="card-img-top" src="https://giffiles.alphacoders.com/132/13200.gif" alt="Card image cap">
         </div>
       </div>
     </div>
